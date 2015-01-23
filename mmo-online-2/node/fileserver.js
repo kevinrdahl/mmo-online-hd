325120393e8e870cef08c67b42fda2347f1db011
Kevin <kevin.of.dahl@gmail.com>
2015-01-22 17:29:46 -0800

    Added WebStorm project
    About a day's work. Probably nonfunctional.

---
+++ mmo-online-2/node/fileserver.js
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 1/20/2015.
+ */
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '../www'));
+
+app.listen(9000);
+console.log('HTTP server running on port 9000.');
