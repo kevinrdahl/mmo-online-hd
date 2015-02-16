fe40a529e3f470a97c5763daf190ab9e733df277
Kevin <kevin.of.dahl@gmail.com>
2015-02-15 21:43:54 -0800

    sick actionbar UI

--- mmo-online-2/node/fileserver.js
+++ mmo-online-2/node/fileserver.js
@@ -4,7 +4,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '../www'));
+app.use(express.static(__dirname + '/../www'));
 
 app.listen(9000);
 console.log('HTTP server running on port 9000.');
+console.log('Serving ' + __dirname + '/../www');
